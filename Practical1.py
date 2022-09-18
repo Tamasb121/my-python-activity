68388ccd957053ed92e7dacb55abd96c24b0f85b
Tamagh <108743811+Tamagh@users.noreply.github.com>
2022-09-18 16:50:34 +0100

    Add files via upload

---
+++ Practical1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# First coding exercise.
+print('my name is Tamas')
+print('the date is 18 September 2022')
+
+
+# In[3]:
+
+
+# Write a program to analyse real estate data
+a, b, c, d, e = 45000, 23400, 67000, 34600, 12900
+# Total sales in December.
+print(a + b + c + d + e, 'Total Sales')
+
+
+# In[8]:
+
+
+
+a, b, c, d, e = 45000, 23400, 67000, 34600, 12900
+# Average sales price in December.
+print((a + b + c + d + e)/5, "Average Sales Price")
+
+
+# In[12]:
+
+
+print((67000 - 6500), "C Property Price")
+
